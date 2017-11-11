09aeb7449f5b1328e2e505b05060dfbceec2e8b7
Noblesse4Razor <noblesse4razor@gmail.com>
2017-11-11 23:01:27 +0300

    project/new-anatomy

---
+++ webpack/rules/css.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const postcssImport = require('postcss-import');
+const postcssCssnext = require('postcss-cssnext');
+const postcssReporter = require('postcss-reporter');
+const PATHS = require('../paths');
+
+module.exports = ({ production = false, browser = false } = {}) => {
+
+  const localIdentName = 'localIdentName=[name]__[local]___[hash:base64:5]';
+
+  const createCssLoaders = embedCssInBundle => ([
+    {
+      loader: embedCssInBundle ? 'css-loader' : 'css-loader/locals',
+      options: {
+        localIdentName,
+        sourceMap: true,
+        modules: true,
+        importLoaders: 1
+      }
+    },
+    {
+      loader: 'postcss-loader',
+      options: {
+        ident: 'postcss',
+        plugins: [
+          postcssImport({ path: path.resolve(PATHS.app, './css') }),
+          postcssCssnext({ browsers: ['> 1%', 'last 2 versions'] }),
+          postcssReporter({ clearMessages: true })
+        ]
+      }
+    }
+  ]);
+
+  const createBrowserLoaders = extractCssToFile => loaders => {
+    if (extractCssToFile) {
+      return ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: loaders
+      });
+    }
+    return [{ loader: 'style-loader' }, ...loaders];
+  };
+
+  const serverLoaders = createCssLoaders(false);
+  const browserLoaders = createBrowserLoaders(production)(createCssLoaders(true));
+
+  return {
+    test: /\.css$/,
+    use: browser ? browserLoaders : serverLoaders,
+    include: PATHS.app
+  };
+};
+
