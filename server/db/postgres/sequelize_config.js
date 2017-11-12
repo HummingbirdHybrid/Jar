23a99328470b678c398a6d28cc01cf3ff296a9f3
Noblesse4Razor <noblesse4razor@gmail.com>
2017-11-12 16:44:32 +0300

    update package.json

--- server/db/postgres/sequelize_config.js
+++ server/db/postgres/sequelize_config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.PGUSER || 'root',
     password: null,
-    database: 'react_webpack_node_development',
+    database: 'react_webpack_node_production',
     host: '127.0.0.1',
     dialect: 'postgres'
   },
