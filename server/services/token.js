204062346e2db54a3213bca2fd30e890229e451b
Noblesse4Razor <noblesse4razor@gmail.com>
2017-11-19 12:45:05 +0300

    babel-loading-fix

--- server/services/token.js
+++ server/services/token.js
@@ -1,37 +1,38 @@
-// import jwt from 'jsonwebtoken';
-// const JWTsecret = 'a4vb8fUu9hEvXfX6szYawQU/b2OE4';
-// const expirationPeriod = '30d';
+import { verify, sign } from 'jsonwebtoken';
 
-// export async function getJWToken(user) {
-//     return await jwt.sign(
-//         {
-//             id: user.id,
-//             name: user.name,
-//         },
-//         JWTsecret,
-//         {
-//             expiresIn: expirationPeriod,
-//         }
-//     );
-// }
+const JWTsecret = 'a4vb8fUu9hEvXfX6szYawQU/b2OE4';
+const expirationPeriod = '30d';
 
-// export async function decodeJWToken(token) {
-//     return await jwt.verify(token, JWTsecret);
-// }
+export async function getJWToken(user) {
+    return sign(
+        {
+            id: user.id,
+            name: user.name,
+        },
+        JWTsecret,
+        {
+            expiresIn: expirationPeriod,
+        }
+    );
+}
 
-// export async function verifyJWToken(token) {
-//     try {
-//         const result = await jwt.verify(token, JWTsecret);
+export async function decodeJWToken(token) {
+    return verify(token, JWTsecret);
+}
 
-//         if(result) {
-//             return true;
-//         }
-//     } catch(error) {
-//         return false;
-//     }
-// }
+export async function verifyJWToken(token) {
+    try {
+        const result = await verify(token, JWTsecret);
 
-// export async function extractIdFromToken(token) {
-//     const decodedData = await decodeJWToken(token);
-//     return decodedData.id;
-// }
+        if(result) {
+            return true;
+        }
+    } catch (error) {
+        return false;
+    }
+}
+
+export async function extractIdFromToken(token) {
+    const decodedData = await decodeJWToken(token);
+    return decodedData.id;
+}
